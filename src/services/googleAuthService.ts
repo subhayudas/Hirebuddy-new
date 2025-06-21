b39cf3afc6e4e362659da354a371c95a2e5fa283
subhayudas <subhayudas49@gmail.com>
2025-06-21 20:45:31 +0530

    pushh

--- src/services/googleAuthService.ts
+++ src/services/googleAuthService.ts
@@ -214,12 +214,9 @@ class GoogleAuthService {
         // Check if token has required scopes
         const requiredScopes = [
           'https://www.googleapis.com/auth/gmail.send',
-          'https://www.googleapis.com/auth/gmail.readonly',
-          'https://www.googleapis.com/auth/gmail.metadata',
           'https://www.googleapis.com/auth/gmail.labels',
           'https://www.googleapis.com/auth/gmail.modify',
           'https://www.googleapis.com/auth/userinfo.email',
-          'https://www.googleapis.com/auth/userinfo.profile',
           'https://www.googleapis.com/auth/contacts.readonly'
         ];
         
